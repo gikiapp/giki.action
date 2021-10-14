55c8b9ea678bec46b5f79ccd4ffbb3ebf8193a87
Minghe Huang <h.minghe@gmail.com>
2021-10-14 23:15:38 +0800

    feat(clean): clean code

--- action.test.js
+++ action.test.js
@@ -2,5 +2,5 @@ const { messageOf } = require("./action");
 
 test("messageOf", async () => {
   const msg = await messageOf("2498a9f3f2a667476bb92072bc0e3fd9634572bc");
-  expect(msg).toEqual("works");
+  expect(msg).toEqual("Create CODEOWNERS");
 });
